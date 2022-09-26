79f0ce63d19e7cabebc3a5c508af81b705ba27eb
HKBingJohn <bingjohn.max@gmail.com>
2022-09-26 11:41:10 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	User/考勤数据预处理.ipynb

--- Work/随机点名小程序.py
+++ Work/随机点名小程序.py
@@ -29,34 +29,30 @@ def main():
     print('输入T测试概率，输入Y开始点名')
     while True:
         a = input('请输入：')
-        if a == 'T':
-            test()
-        elif a == 'Y':
+        if a == 'T' or 't':
+            df = pd.DataFrame(stu)
+            df['count'] = 0
+
+            print('测试概率')
+            print('输入数字N，测试N次随机点名的概率')
+            while True:
+                num = input('请输入：')
+                if num.isdigit():
+                    num = int(num)
+                    break
+                else:
+                    print('输入错误，请重新输入！')
+            for i in range(num):
+                name = random.choice(stu)
+                df.loc[df[0] == name, 'count'] += 1
+            df['probability'] = df['count'] / num
+            print(df)
+            print('测试完毕！')
+        elif a == 'Y' or 'y':
             while True:
                 print(random.choice(stu))
                 if input("q结束，其他键回车继续") == "q":
                     break
-
-
-def test():
-    df = pd.DataFrame(stu)
-    df['count'] = 0
-
-    print('测试概率')
-    print('输入数字N，测试N次随机点名的概率')
-    while True:
-        num = input('请输入：')
-        if num.isdigit():
-            num = int(num)
-            break
-        else:
-            print('输入错误，请重新输入！')
-    for i in range(num):
-        name = random.choice(stu)
-        df.loc[df[0] == name, 'count'] += 1
-    df['probability'] = df['count'] / num
-    print(df)
-    print('测试完毕！')
 
 
 if __name__ == '__main__':
