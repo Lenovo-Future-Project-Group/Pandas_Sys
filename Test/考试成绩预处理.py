ca769933d7cf3649ecbce430625a0118a60f6c93
HKBingJohn <bingjohn.max@gmail.com>
2022-09-27 21:41:36 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	User/考勤数据预处理.ipynb

--- Test/考试成绩预处理.py
+++ Test/考试成绩预处理.py
@@ -1,20 +1,26 @@
-import re
+import os
 import pandas as pd
 
-name = '北京市昌平职业学校2021-2022春学期_信息技术系_大数据201-202班_互联网数据采集_期末考试成绩.xlsx'
+path = '../Data/inputData/考试成绩数据/'
+files = os.listdir(path)
+result_df = pd.DataFrame()
 
-# 使用正则表达式匹配'北京市昌平职业学校2021-2022春学期_信息技术系_大数据201-202班_互联网数据采集_期末考试成绩.xlsx'中的 '互联网数据采集_期末考试成绩'
-# 用于后面的文件重命名
-# re.findall(r'_(.*?)_', name)
-# ['互联网数据采集', '期末考试成绩']
-# re.findall(r'_(.*?)_', name)[0]
-# '互联网数据采集'
-# re.findall(r'_(.*?)_', name)[1]
-# '期末考试成绩'
-# re.findall(r'_(.*?)_', name)[0] + re.findall(r'_(.*?)_', name)[1]
+# cj = pd.read_excel(path + '北京市昌平职业学校2021-2022春学期_信息技术系_大数据201-202班_互联网数据采集_期末考试成绩.xlsx')
+# cj.index = cj['姓名']
+# fx = pd.read_excel(path + '北京市昌平职业学校2021-2022春学期_信息技术系_大数据201-202班_大数据分析与实现_期末考试成绩.xlsx')
+# fx.index = fx['姓名']
+# sh = pd.read_excel(path + '北京市昌平职业学校2021-2022春学期_信息技术系_大数据201-202班_数据可视化设计与实现_期末考试成绩.xlsx')
+# sh.index = sh['姓名']
+# sy = pd.read_excel(path + '北京市昌平职业学校2021-2022春学期_信息技术系_大数据201-202班_职业素养_期末考试成绩_唐大成.xlsx')
+# sy.index = fx['姓名']
 
-# 创建dataframe并将索引修改成‘naem’
-df = pd.read_excel(name)
-df.index = df['name']
+# result_df['互联网数据采集'] = cj['总成绩']
+# result_df['大数据分析与实现'] = fx['总成绩']
+# result_df['数据可视化设计与实现'] = sh['总成绩']
+# result_df['职业素养'] = sy['总成绩']
+# result_df.to_csv('inputData/考试成绩数据/考勤成绩数据.csv', encoding='utf_8', index=True)
 
-
+# 将 cj fx sh sy 转换为动态变量
+loc = locals()
+for file in files:
+    if file.endswith('xlsx'):
